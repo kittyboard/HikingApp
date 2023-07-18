80d24c95b779981c39510bcf1f154f575bc073b8
kittyboard <76805188+kittyboard@users.noreply.github.com>
2023-07-18 10:00:16 +0900

    Initial Commit

---
+++ HikingApp/HikingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingAppApp.swift
+//  HikingApp
+//
+//  Created by 통이 on 2023/07/18.
+//
+
+import SwiftUI
+
+@main
+struct HikingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
